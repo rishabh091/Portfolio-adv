0020e531b6cdb857517003967e2343343fbc7141
Rishabh <rishabh@felixhealthcare.ai>
2021-07-30 17:04:58 +0530

    front completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,17 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  ngAfterViewInit() {
+  }
+
+}
