bbeff1d41538dc541244e6cc9e1a347e31d0544a
Rishabh <rishabh@felixhealthcare.ai>
2021-08-07 15:55:49 +0530

    added adpative experience

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
+  experience: string
+
   constructor() { }
 
   ngOnInit() {
+    const joiningDate = new Date(2020, 7, 5)
+    const currentDate = new Date()
+
+    const utcCurrentDate = Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate())
+    const utcJoiningDate = Date.UTC(joiningDate.getFullYear(), joiningDate.getMonth(), joiningDate.getDate()) 
+
+    const diffDays = Math.floor((utcCurrentDate - utcJoiningDate) / (1000 * 60 * 60 * 24))
+    this.experience = Math.floor(diffDays / 365) + '+'
   }
 
 }
